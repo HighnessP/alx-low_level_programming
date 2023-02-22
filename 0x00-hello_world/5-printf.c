ad37d6d4ab8eb0e4fe600e808894b069393d55d1
Highness <debapreya@gmail.com>
2023-02-21 17:15:55 -0800

    hello world files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,12 +10,7 @@ nclude<stdio.h>
  *
  * */
 
-int main(void)
-
-{
-
-	        printf("with proper grammar, but the outcome is a piece of art,\n");
-
-		        return (0);
-
+int main(void){
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+       	return (0);
 }
